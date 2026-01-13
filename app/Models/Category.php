87ef9c866ead960c3bcadbcb65878f05e7ffcdde
IlyassLho <ilyasslhouari@gmail.com>
2026-01-13 01:08:19 +0100

    Add Category model with fillable attributes and relationship to products

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'restaurant_id',
+        'name'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
