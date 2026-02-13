f0ab84e178a16b0033aee2913532aed2e09298cd
IlyassLho <ilyasslhouari@gmail.com>
2026-02-14 00:24:53 +0100

    feat(front): integrate Tailwind CSS and set up Toast notifications

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import App from './App.jsx'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+        <App />
+        <ToastContainer position="bottom-right" /> {/* Notifications */}
+      </BrowserRouter>
   </StrictMode>,
 )
