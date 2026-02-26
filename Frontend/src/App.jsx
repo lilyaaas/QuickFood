8879f111929bdd7e214a8c0019e49cf8f12a07c0
lilyaaas <ilyasslhouari@gmail.com>
2026-02-26 18:21:01 +0000

    Refactor(front):  App component to implement routing structure with MainLayout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import RestaurantDetails from './pages/RestaurantDetails';
+import Cart from './pages/Cart';
+import RestaurantList from './components/Restaurant/RestaurantList';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
+
   return (
-    <div>
-      <h1>QuickBite Frontend 🚀</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="restaurants" element={<RestaurantList />} />
+        <Route path="restaurants/:id/products" element={<RestaurantDetails />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
